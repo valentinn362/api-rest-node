37dc839fb6b511c5be5e34b4ec0a8f33ea380801
valentin362 <alvezrojasvalentin@gmail.com>
2026-02-16 15:06:17 -0300

    feat: inicializa servidor Express con configuración base

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const { connection } = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 //Inicializar app
 console.log("App de node arrancada");
@@ -8,3 +10,19 @@ console.log("App de node arrancada");
 // Conecatar a la base de datos
 connection();
 
+// Crear servidor Node
+const app = express(); // variable en la cual se guarda la ejecucion de express
+const puerto = 3900;
+
+// Configurar cors
+app.use(cors()); // middleware del cors para permitir peticiones desde otros dominios
+
+// Convertir body a objeto js
+app.use(express.json()); // middleware, si le paso distintos datos en un post, luego si los quiero usar dentro de mi ruta, voy a tener un objeto js usable en mi codigo 
+
+// Crear rutas
+
+// Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en el puerto: "+puerto);
+});
